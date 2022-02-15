07bc42ef6a7d2fcea99a1d3a94f006295853d04a
haithem <haithem2001@gmail.com>
2022-02-15 18:40:12 +0100

    log

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import dotenv from 'dotenv'; dotenv.config();
 
 const app = fastify();
 
+
+
 app.addHook('onRequest', async (req, reply) => {
     // @ts-ignore
     const path = req.params['*']
@@ -18,5 +20,8 @@ app.addHook('onRequest', async (req, reply) => {
 app.get('/', (req, rep) => {
     rep.send(200)
 })
+
+
+console.log(ip.address())
 
 app.listen(process.env.port || 80, process.env.host || ip.address(), () => console.log("server started"));
