c7af0cb5cd7224de0b2d98e0b46cde0eaa392685
haithem <haithem2001@gmail.com>
2022-02-15 18:53:35 +0100

    e

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import fastify from "fastify";
+import http from "http";
 
 import ip from "ip";
 
@@ -6,22 +7,28 @@ import dotenv from 'dotenv'; dotenv.config();
 
 const app = fastify();
 
+async function setup() {
+    console.log(ip.address())
+    app.addHook('onRequest', async (req, reply) => {
+        // @ts-ignore
+        const path = req.params['*']
+        // @ts-ignore
+        const port = req.query.port || req.query.p;
 
+        reply.redirect(new URL(path || "", `http://${ip.address()}:${port}`).toString());
+    })
 
-app.addHook('onRequest', async (req, reply) => {
-    // @ts-ignore
-    const path = req.params['*']
-    // @ts-ignore
-    const port = req.query.port || req.query.p;
+    app.get('/', (req, rep) => {
+        rep.send(200)
+    })
 
-    reply.redirect(new URL(path || "", `http://${ip.address()}:${port}`).toString());
-})
+    // http.createServer().on('request',app)
+    try {
+        await app.listen(process.env.port || 80, process.env.host || ip.address(), () => console.log("server started"))
+    } catch (err) {
+        console.log(err)
+    }
 
-app.get('/', (req, rep) => {
-    rep.send(200)
-})
+}
 
-
-console.log(ip.address())
-
-app.listen(process.env.port || 80, process.env.host || ip.address(), () => console.log("server started"));
+setup();
